4d3476e5ac773a23af7b924c10fa532b6946a9b6
Gaurav Kandlikar <gkan@umd.edu>
2015-02-08 12:12:54 -0500

    added new function to server.R to do lagged logistic. needs to be shinyfied

--- single_pop/server.R
+++ single_pop/server.R
@@ -1,9 +1,50 @@
+
+# Define lagged logistic growth
+# defining a new custom function that can do exponential growth, logistic growth and lagged logistic growth. Function works, but need to make it shiny-compatible.
+# All encompassing single population function
+# single.pop <- function(N=10,time=500,k=-9999,tau=0,r=.2) {
+#   
+#   vect <- numeric()
+#   vect[1] <- N
+#   
+#   ## Do this if no carrying capacity
+#   if (k == -9999) {
+#     for (i in 2:time) {vect[i] <- vect[i-1]+(vect[i-1]*r)}
+#   }
+#   
+#   ## Do this if carrying capacity defined
+#   if (k != -9999) {
+#     
+#     ## Do this if tau is zero (no lag)
+#     if (tau == 0){
+#       for (i in 2:time){vect[i] <- vect[i-1]+ (vect[i-1]*r*(1-(vect[i-1]/k)))}
+#     }
+#     
+#     ## Do this if tau is not zero (there is some lag)
+#     if (tau != 0) {
+#       for (i in 2:time) {
+#         ## If time is less than lag, just do logistic growth
+#         if (i <= tau) {vect[i] <- vect[i-1] + (vect[i-1]*r*(1- (vect[i-1]/k)))}
+#         ## If time is greater than lag, implement the lag
+#         else {vect[i] <- vect[i-1]+(vect[i-1]*r*(1- (vect[i-tau]/k)))}
+#         
+#       }
+#     }
+#     
+#   }  
+#   return(vect)
+#   
+# }
+
+### Back to the normal stuff
+
 library(deSolve) # package for solving diferential equation
 library(dygraphs) # package for interactive plots
 library(xts) # package for timeseries analysis
 ########################################################## 
 # Global options - define the population growth function #
 ########################################################## 
+
 
 growth <- function(time,init,params) {
   with (as.list(c(time,init,params)), {
@@ -37,7 +78,9 @@ shinyServer(
     
     output$UI1 <- renderUI({
       if (input$Density == 2){
-        numericInput("K",label=h6("Select a value for K (carrying capacity)."),value=500,min=1)}
+        numericInput("K",label=h6("Select a value for K (carrying capacity)."),value=500,min=1)#,
+        #numericInput("tau",label=h6("Select a value for tau (time lag)."),value=0)
+      }
       else {""}
     })
     
