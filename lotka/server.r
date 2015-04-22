54905db742d09276f91c544c17fbedbfa0e75f4a
Gaurav Kandlikar <gkan@umd.edu>
2015-04-22 11:50:33 -0400

    commiting changes to lotka app

---
+++ lotka/server.r
@@ -0,0 +1,120 @@
+# Lotka
+# server.R
+
+library(deSolve)
+
+lv <- function(time,init,params) {
+  with (as.list(c(time,init,params)), {
+    # description of parameters:
+    # r1 = growthrate of Sp. 1; r2 = growthrate of Sp. 2
+    # N = population size of Sp. 1; P = Population Size Sp. 2
+    # a = competitive impact of Sp. 2 on Sp. 1; b = competitive impact of Sp 1 on Sp 2
+    # K1/K2 = carrying capacities
+    dN1dt <- (r1*N1*(1-(N1+a*N2)/K1)) 
+    dN2dt <- (r2*N2*(1-(N2+b*N1)/K2))
+    return(list(c(dN1dt,dN2dt)))
+  })
+}
+
+# Root functions define the stopping parameters - when this root is zero, the thing will stop.
+rootfun <- function(Time, State, Pars) {
+  dstate <- unlist(lv(Time, State, Pars))
+  sum(abs(dstate)) - 1e-3
+}
+
+shinyServer(
+  
+  function(input, output) {
+    # randomly select starting populations each time a new session is launched
+    p <- runif(n=2,min=1,max=1000)
+    
+    
+    #############################################################################################
+    # Reactively generate the parameters and the dataframe based on user inputs
+    #############################################################################################
+    
+    # Reactively generate UI- if user selects "run till specified time" then let them choose steps.
+    output$UI1 <- renderUI({
+      if (input$iter == 2){
+        numericInput("steps",label=h6("Time steps to run the model"),value=100,min=1)}
+      else {""}
+      
+    })
+    
+    # Reactively generate initial conditions. Access later using init()
+    init <- reactive ({
+      c("N1" = input$n1, "N2" = input$n2)
+    })
+    
+    # Reactively generate the params list. Access later using params()
+    params <- reactive({
+      c("r1" = 1, "r2" = 1, "K1" = as.numeric(input$k1), "K2" = as.numeric(input$k2), "a" = as.numeric(input$alpha), "b" = as.numeric(input$beta))
+    })
+    
+    # Reactively generate the time. Access later using time()
+    time <- reactive({
+      if (input$iter == 2){seq(0,input$steps,by=1)}
+      else {seq(0,1000, by=1)}
+    })
+    
+    # Reactively generate the lvout dataframe. Access later using lvout()
+    lvout <- reactive({
+      init<- init()
+      params <- params()
+      time <- time()
+      
+      if (input$iter == 2){floor(as.data.frame(ode(func=lv,y=init,parms=params,times=time)))}
+      else {floor(as.data.frame(lsodar(func=lv,y=init,parms=params,times=time,rootfun=rootfun)))}
+    })
+    
+    
+    
+    
+    
+    
+    
+    #############################################################################################
+    # Generate the output table graphs!
+    #############################################################################################    
+    
+    # Generate population table
+    output$table1<- renderTable({      
+      generated_df <- lvout()
+      generated_df
+    })
+    
+    # Generate the plots
+    output$plot1<- renderPlot({
+      init<- init()
+      params <- params()
+      time <- time()
+      generated_df <- lvout()      
+      
+      par(mfrow=c(2,2))
+      # plot the Zero Net Growth Isoclines based on parameters above.
+      plot(1,type="n",xlim=c(0,max(params["K1"],params["K2"]/params["b"])*1.25),
+           ylim=c(0,max(params["K2"],params["K1"]/params["a"])*1.25),
+           xlab  ="Species 1",ylab="Species 2",main="ZNGIs for Sp.1 and Sp.2",
+           xaxs="i",yaxs="i",cex.axis=1.5,cex.lab=1.5,cex.main=1.5)
+      lines(x = c(params["K1"],0),y = c(0,params["K1"]/params["a"]),lwd=2,col="blue")
+      lines(x = c(params["K2"]/params["b"], 0) ,y = c(0,params["K2"]),lwd=2,col="red")
+      
+      # plot the starting population size
+      points(x=init["N1"],y=init["N2"],cex=2,pch=20)
+      
+      # Plot out the results of the ODE. 
+      # First plot is for N vs P (sp1 v sp2)
+      plot(generated_df$N2~generated_df$N1,type="o",xlim=c(0,max(params["K1"],params["K2"]/params["b"])*1.25),
+           ylim=c(0,max(params["K2"],params["K1"]/params["a"])*1.25), main = "Trajectory of population sizes",xlab="Species 1", 
+           ylab="Species 2",cex.axis=1.5,cex.lab=1.5,cex.main=1.5, col=rainbow(nrow(generated_df)))
+      
+      # Second plot for N & P v Time
+      plot(generated_df$N1,ylim=c(0,max(max(generated_df$N1),max(generated_df))*1.25),ylab="Population size",
+           type="l",lwd=2, col="blue",cex.axis=1.5,cex.lab=1.5,
+           main="Population size vs time",cex.main=1.5)
+      points(generated_df$N2,col="red",type="l",lwd=2)
+      
+    })
+    
+  }
+)
---
+++ lotka/ui.r
@@ -0,0 +1,63 @@
+library(shinythemes) # make things look pretty
+
+
+shinyUI(fluidPage(
+  theme = shinytheme("flatly"),
+  
+  titlePanel("Modeling Lotka Volterra competition"),
+  
+  sidebarLayout(
+    sidebarPanel( # This starts creating the left-hand panel 
+      title="Lotka Volterra Competition", # Adds 
+      
+      helpText(h4("Set initial population sizes")),
+      numericInput("n1",label=p("Select a value for", span("N1",style = "color:blue")),value=floor(rnorm(1,mean=500,sd=100)),min=1),
+      numericInput("n2",label=p("Select a value for", span("N2",style = "color:red" )),value=ceiling(rnorm(1,mean=500,sd=100)),min=1),
+      
+      helpText(h4("Set carrying capacities")),
+      numericInput("k1",label=p("Select a value for", span("K1",style = "color:blue")),value=500,min=1),
+      numericInput("k2",label=p("Select a value for", span("K2",style = "color:red" )),value=500,min=1),
+      
+      
+      helpText(h4("Set competition coefficients")),
+      sliderInput("alpha", 
+                  label = p("Choose a value for", HTML("&alpha;")),
+                  min = 0, max = 2.0, value=0.75, step = 0.01),
+      sliderInput("beta", 
+                  label = p("Choose a value for", HTML("&beta;")),
+                  min = 0, max = 2.0, value=0.75, step = 0.01),
+      
+      br(),
+      radioButtons("iter", label="",
+                   choices = list("Run until steady state" = 1,
+                                  "Run for specified length" = 2),
+                   selected = 1),
+      htmlOutput("UI1")
+      
+    ),  
+    
+    mainPanel(
+      tabsetPanel(
+        tabPanel(title = "Equation",
+                 h4("These are the Lotka-Volterra equations for two species:"),
+                 p(withMathJax("$$ \\frac{dN_{1}}{dt} = (r_{1}N_{1}) \\left( 1-\\frac{N_{1} + \\alpha*N_{2}}{K_{1}} \\right) $$")),
+                 p(withMathJax("$$ \\frac{dN_{2}}{dt} = (r_{2}N_{2}) \\left( 1-\\frac{N_{2} + \\beta*N_{1}}{K_{2}} \\right) $$")),
+                 br(),
+                 h4("These equations calculate equilibrium values of N1 and N2:"),
+                 p(withMathJax("$$ \\frac{dN_{1}}{dt} = 0 \\text{  when  } N_{1} = K_{1} - \\alpha*N_{2} $$")),
+                 p(withMathJax("$$ \\frac{dN_{2}}{dt} = 0 \\text{  when  } N_{2} = K_{2} - \\beta*N_{1} $$"))                 
+        ),
+        
+        tabPanel(title = "Plots",                 
+                 h4("These plots show changes in population sizes with time"),
+                 plotOutput("plot1",height="700px",width="700px")
+        ),
+        
+        tabPanel(title = "Tables",
+                 h4("This table shows the population sizes at each time step"),
+                 tableOutput("table1")
+        )
+      )
+    )
+  )
+))
